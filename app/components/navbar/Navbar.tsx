644d16bf4fbdfc06efcc88f7110da9da7118f167
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-04-02 15:27:53 +0300

    Container
    container

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,8 +1,15 @@
+import Container from "../Container";
 
 const Navbar = () => {
   return (
-    <div>
-    I am a Navbar
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+        <div
+            className="py-4 border-b-[1px]"
+        >
+            <Container>
+                
+            </Container>
+        </div>
     </div>
   )
 }
