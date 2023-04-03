742e871845dca1cbb7f65c9f7c486c297588606d
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-04-03 14:23:23 +0300

    Update Search.tsx
    nabvar elements

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { BiSearch } from 'react-icons/bi';
+
 const Search = () => {
     return (
         <div
@@ -24,13 +26,52 @@ const Search = () => {
                 "
             >
                 <div
-                     className="
+                    className="
                      text-sm 
                      font-semibold 
                      px-6
                    "
                 >
                     Anywhere
+                </div>
+                <div
+                    className="
+                     hidden 
+                     sm:block 
+                     text-sm 
+                     font-semibold 
+                     px-6 
+                     border-x-[1px] 
+                     flex-1 
+                     text-center
+                   "
+                >
+                    Anyweek
+                </div>
+                <div
+                    className="
+                    text-sm 
+                    pl-6 
+                    pr-2 
+                    text-gray-600 
+                    flex 
+                    flex-row 
+                    items-center 
+                    gap-3
+                  "
+                >
+
+                    <div className="hidden sm:block">Add guest</div>
+                    <div
+                        className="
+                    p-2 
+                    bg-rose-500 
+                    rounded-full 
+                    text-white
+                  "
+                    >
+                        <BiSearch size={18} />
+                    </div>
                 </div>
             </div>
 
