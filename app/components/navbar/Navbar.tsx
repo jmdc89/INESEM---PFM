7117a19a9aa0dd4fdf0d7921efbc379b3824646e
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-04-03 15:09:50 +0300

    UserMenu
    component

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
+import UserMenu from "./UserMenu";
 
 const Navbar = () => {
   return (
@@ -21,6 +22,7 @@ const Navbar = () => {
                 >
                     <Logo />
                     <Search />
+                    <UserMenu />
                 </div>
             </Container>
         </div>
