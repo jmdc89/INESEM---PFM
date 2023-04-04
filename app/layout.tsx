d64d61f26f0ddf5ce22fce2d10f764def0324dcb
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-04-04 13:53:51 +0300

    Update layout.tsx
    prop

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal />
+          <Modal actionLabel="Submit" title="Hello World" isOpen />
           <Navbar />
         </ClientOnly>
         {children}
