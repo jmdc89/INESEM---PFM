43fc4e7363c7db3a7874d64ad34c9c6d34233a2e
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-04-04 13:21:06 +0300

    Update Button.tsx
    interface

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,3 +1,15 @@
+'use client';
+
+import { IconType } from "react-icons";
+
+interface ButtonProps {
+  label: string;
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  disabled?: boolean;
+  outline?: boolean;
+  small?: boolean;
+  icon?: IconType;
+}
 
 const Button = () => {
     return (
