fdf186b1a52cf988689677d0e730fc889e846bdd
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-04-04 13:22:38 +0300

    Update Button.tsx
    Button props

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -3,15 +3,22 @@
 import { IconType } from "react-icons";
 
 interface ButtonProps {
-  label: string;
-  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
-  disabled?: boolean;
-  outline?: boolean;
-  small?: boolean;
-  icon?: IconType;
+    label: string;
+    onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    disabled?: boolean;
+    outline?: boolean;
+    small?: boolean;
+    icon?: IconType;
 }
 
-const Button = () => {
+const Button: React.FC<ButtonProps> = ({
+    label,
+    onClick,
+    disabled,
+    outline,
+    small,
+    icon: Icon,
+}) => {
     return (
         <button></button>
     );
