8d8073ef909b457c7e041f734d64d7b2ed8552e2
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-04-06 07:49:22 +0300

    Update RegisterModal.tsx
    onSubmit

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -31,6 +31,21 @@ const RegisterModal = () => {
         },
       });
 
+      const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        setIsLoading(true);
+    
+        axios.post('/api/register', data)
+        .then(() => {
+           registerModal.onClose();
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        })
+      }
+
     return (
         <div></div>
     );
