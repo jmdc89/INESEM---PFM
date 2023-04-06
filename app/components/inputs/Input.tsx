2369a9e2a44dbbef36002add6fbdb24721c4c993
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-04-06 13:20:03 +0300

    Update Input.tsx
    imports and interfaces

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -1,6 +1,33 @@
 'use client';
 
-const Input = () => {
+import { 
+    FieldErrors, 
+    FieldValues, 
+    UseFormRegister 
+  } from "react-hook-form";
+  import { BiDollar } from "react-icons/bi";
+  
+  interface InputProps {
+    id: string;
+    label: string;
+    type?: string;
+    disabled?: boolean;
+    formatPrice?: boolean;
+    required?: boolean;
+    register: UseFormRegister<FieldValues>,
+    errors: FieldErrors
+  }
+
+  const Input: React.FC<InputProps> = ({
+    id,
+    label,
+    type = "text", 
+    disabled, 
+    formatPrice,
+    register,
+    required,
+    errors,
+  }) => {
     return (
         <div>
 
