e123c54d6d028082a8dbb7d179027bb515ac387a
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-04-17 14:10:01 +0300

    Update LoginModal.tsx
    Translation

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -70,8 +70,8 @@ const LoginModal = () => {
   const bodyContent = (
     <div className="flex flex-col gap-4">
       <Heading
-        title="Welcome back"
-        subtitle="Login to your account!"
+        title="Bienvenido de nuevo"
+        subtitle="Login en tu cuenta!"
       />
       <Input
         id="email"
@@ -98,19 +98,19 @@ const LoginModal = () => {
       <hr />
       <Button 
         outline 
-        label="Continue with Google"
+        label="Continua con Google"
         icon={FcGoogle}
         onClick={() => signIn('google')}
       />
       <Button 
         outline 
-        label="Continue with Github"
+        label="Continua con Github"
         icon={AiFillGithub}
         onClick={() => signIn('github')}
       />
       <div className="
       text-neutral-500 text-center mt-4 font-light">
-        <p>First time using Airbnb?
+        <p>Primera vez usando Inesem?
           <span 
             onClick={onToggle} 
             className="
@@ -118,7 +118,7 @@ const LoginModal = () => {
               cursor-pointer 
               hover:underline
             "
-            > Create an account</span>
+            > Crea una cuenta</span>
         </p>
       </div>
     </div>
