fe479c3769200997bb00fc1b64ba1d1c9910a820
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-04-17 13:39:43 +0300

    Update [...nextauth].ts
    imports

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,15 @@
+import bcrypt from "bcrypt"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import NextAuth, { AuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
+
+import prisma from "@/app/libs/prismadb"
+
+export const authOptions: AuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+
+    ]
+}
