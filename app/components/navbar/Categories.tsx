972d79a81ad42fbb3d6044be6a99fcba0cade297
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-04-19 15:54:10 +0300

    Update Categories.tsx
    categories const

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -19,6 +19,85 @@ import { MdOutlineVilla } from 'react-icons/md';
 
 import Container from '../Container';
 
+export const categories = [
+    {
+      label: 'Playa',
+      icon: TbBeach,
+      description: '¡Esta propiedad está cerca de la playa!',
+    },
+    {
+      label: 'Molinos de viento',
+      icon: GiWindmill,
+      description: '¡Esta propiedad tiene molinos de viento!',
+    },
+    {
+      label: 'Moderno',
+      icon: MdOutlineVilla,
+      description: '¡Esta propiedad es moderna!'
+    },
+    {
+      label: 'Campo',
+      icon: TbMountain,
+      description: '¡Esta propiedad está en el campo!'
+    },
+    {
+      label: 'Piscinas',
+      icon: TbPool,
+      description: '¡Esta propiedad tiene una hermosa piscina!'
+    },
+    {
+      label: 'Islas',
+      icon: GiIsland,
+      description: '¡Esta propiedad está en una isla!'
+    },
+    {
+      label: 'Lago',
+      icon: GiBoatFishing,
+      description: '¡Esta propiedad está cerca de un lago!'
+    },
+    {
+      label: 'Esquí',
+      icon: FaSkiing,
+      description: '¡Esta propiedad tiene actividades de esquí!'
+    },
+    {
+      label: 'Castillos',
+      icon: GiCastle,
+      description: '¡Esta propiedad es un antiguo castillo!'
+    },
+    {
+      label: 'Cuevas',
+      icon: GiCaveEntrance,
+      description: '¡Esta propiedad está en una cueva espeluznante!'
+    },
+    {
+      label: 'Camping',
+      icon: GiForestCamp,
+      description: '¡Esta propiedad ofrece actividades de camping!'
+    },
+    {
+      label: 'Ártico',
+      icon: BsSnow,
+      description: '¡Esta propiedad está en un ambiente ártico!'
+    },
+    {
+      label: 'Desierto',
+      icon: GiCactus,
+      description: '¡Esta propiedad está en el desierto!'
+    },
+    {
+      label: 'Granja',
+      icon: GiBarn,
+      description: '¡Esta propiedad está en una granja!'
+    },
+    {
+      label: 'Lujo',
+      icon: IoDiamond,
+      description: '¡Esta propiedad es nueva y lujosa!'
+    }
+  ]
+  
+
 const Categories = () => {
     return (
         <Container>
