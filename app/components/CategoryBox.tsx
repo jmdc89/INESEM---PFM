95a9e9a5c8ed889311b1a211e4e27cd20ee64e24
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-04-19 16:01:35 +0300

    Update CategoryBox.tsx
    interface and imports

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -1,5 +1,15 @@
 'use client'
 
+import { useRouter, useSearchParams } from "next/navigation";
+import { useCallback } from "react";
+import { IconType } from "react-icons";
+
+interface CategoryBoxProps {
+    icon: IconType,
+    label: string;
+    selected?: boolean;
+  }
+
 const CategoryBox = () => {
     return (
         <div></div>
