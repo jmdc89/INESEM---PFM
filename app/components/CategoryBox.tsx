02182d111f7d7795bac846d1297e92b49ddeb759
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-04-21 12:11:33 +0300

    Update CategoryBox.tsx
    styles

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -15,7 +15,20 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
 }) => {
     return (
         <div
-            
+        className={`
+        flex 
+        flex-col 
+        items-center 
+        justify-center 
+        gap-2
+        p-3
+        border-b-2
+        hover:text-neutral-800
+        transition
+        cursor-pointer
+        ${selected ? 'border-b-neutral-800' : 'border-transparent'}
+        ${selected ? 'text-neutral-800' : 'text-neutral-500'}
+      `} 
         >
 
         </div>
