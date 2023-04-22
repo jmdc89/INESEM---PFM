b90f0cd39519b420624d3bb86f023707ba4bf338
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-04-22 13:07:25 +0300

    Update RegisterModal.tsx
    OnToggle function

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -4,8 +4,8 @@ import axios from "axios";
 import { AiFillGithub } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
 import { useCallback, useState } from "react";
-import { 
-  FieldValues, 
+import {
+  FieldValues,
   SubmitHandler,
   useForm
 } from "react-hook-form";
@@ -17,124 +17,131 @@ import Input from "../inputs/Input";
 import { toast } from "react-hot-toast";
 import Button from "../Button";
 import { signIn } from "next-auth/react";
+import useLoginModal from "@/app/hooks/useLoginModal";
 
 const RegisterModal = () => {
 
-    const registerModal = useRegisterModal();
-    const [isLoading, setIsLoading] = useState(false);
+  const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
+  const [isLoading, setIsLoading] = useState(false);
 
-    const { 
-        register, 
-        handleSubmit,
-        formState: {
-          errors,
-        },
-      } = useForm<FieldValues>({
-        defaultValues: {
-          name: '',
-          email: '',
-          password: ''
-        },
-      });
+  const {
+    register,
+    handleSubmit,
+    formState: {
+      errors,
+    },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: ''
+    },
+  });
 
-      const onSubmit: SubmitHandler<FieldValues> = (data) => {
-        setIsLoading(true);
-    
-        axios.post('/api/register', data)
-        .then(() => {
-           registerModal.onClose();
-        })
-        .catch((error) => {
-          toast.error('Algo ha ido mal');
-        })
-        .finally(() => {
-          setIsLoading(false);
-        })
-      }
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
 
-      const bodyContent = (
-        <div className="flex flex-col gap-4">
-          <Heading
-            title="Bienvenido a Inesem"
-            subtitle="Crea una cuenta!"
-          />
-          <Input
-            id="email"
-            label="Email"
-            disabled={isLoading}
-            register={register}
-            errors={errors}
-            required
-          />
-          <Input
-            id="name"
-            label="Nombre"
-            disabled={isLoading}
-            register={register}
-            errors={errors}
-            required
-          />
-          <Input
-            id="password"
-            type="password"
-            label="Password"
-            disabled={isLoading}
-            register={register}
-            errors={errors}
-            required
-          />
-        </div>
-      )
+    axios.post('/api/register', data)
+      .then(() => {
+        registerModal.onClose();
+      })
+      .catch((error) => {
+        toast.error('Algo ha ido mal');
+      })
+      .finally(() => {
+        setIsLoading(false);
+      })
+  }
 
-      const footerContent = (
-        <div className="flex flex-col gap-4 mt-3">
-          <hr />
-          <Button 
-            outline 
-            label="Continua con Google"
-            icon={FcGoogle}
-            onClick={() => signIn('google')} 
-          />
-          <Button 
-            outline 
-            label="Continua con Github"
-            icon={AiFillGithub}
-            onClick={() => signIn('github')}
-          />
-          <div 
-            className="
+  const onToggle = useCallback(() => {
+    registerModal.onClose();
+    loginModal.onOpen();
+  }, [registerModal, loginModal])
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Heading
+        title="Bienvenido a Inesem"
+        subtitle="Crea una cuenta!"
+      />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="name"
+        label="Nombre"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        type="password"
+        label="Password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  )
+
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        outline
+        label="Continua con Google"
+        icon={FcGoogle}
+        onClick={() => signIn('google')}
+      />
+      <Button
+        outline
+        label="Continua con Github"
+        icon={AiFillGithub}
+        onClick={() => signIn('github')}
+      />
+      <div
+        className="
               text-neutral-500 
               text-center 
               mt-4 
               font-light
             "
-          >
-            <p>Ya tienes una cuenta?
-              <span 
-                onClick={() => {}}
-                className="
+      >
+        <p>Ya tienes una cuenta?
+          <span
+            onClick={onToggle}
+            className="
                   text-neutral-800
                   cursor-pointer 
                   hover:underline
                 "
-                > Log in</span>
-            </p>
-          </div>
-        </div>
-      )
+          > Log in</span>
+        </p>
+      </div>
+    </div>
+  )
 
-    return (
-        <Modal
-          disabled={isLoading}
-          isOpen={registerModal.isOpen}
-          title="Register"
-          actionLabel="Continue"
-          onClose={registerModal.onClose}
-          onSubmit={handleSubmit(onSubmit)}
-          body={bodyContent}
-          footer={footerContent}
-        />
-    );
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title="Register"
+      actionLabel="Continue"
+      onClose={registerModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
+      footer={footerContent}
+    />
+  );
 }
 
 export default RegisterModal;
