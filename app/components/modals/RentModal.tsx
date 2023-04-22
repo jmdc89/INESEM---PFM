1b90314ed3a60d924ee95c44f9f1fa7a08e3e868
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-04-22 14:15:57 +0300

    Update RentModal.tsx
    enum STEPS

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,11 +1,24 @@
 'use client';
 
+import {useState } from "react";
+
 import useRentModal from "@/app/hooks/useRentModal";
 import Modal from "./Modal"
+
+enum STEPS {
+    CATEGORY = 0,
+    LOCATION = 1,
+    INFO = 2,
+    IMAGES = 3,
+    DESCRIPTION = 4,
+    PRICE = 5,
+  }
 
 
 const RentModal = () => {
     const rentModal = useRentModal();
+
+    const [step, setStep] = useState(STEPS.CATEGORY);
 
     return(
     <Modal
