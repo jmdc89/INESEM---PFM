4b246aff01af12bcf06a6c4f30306bdbeb073819
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-04-22 14:19:44 +0300

    Update RentModal.tsx
    actionLevel steps

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import {useState } from "react";
+import { useMemo, useState } from "react";
 
 import useRentModal from "@/app/hooks/useRentModal";
 import Modal from "./Modal"
@@ -12,7 +12,7 @@ enum STEPS {
     IMAGES = 3,
     DESCRIPTION = 4,
     PRICE = 5,
-  }
+}
 
 
 const RentModal = () => {
@@ -20,14 +20,39 @@ const RentModal = () => {
 
     const [step, setStep] = useState(STEPS.CATEGORY);
 
-    return(
-    <Modal
-        isOpen={rentModal.isOpen}
-        onClose={rentModal.onClose}
-        onSubmit={rentModal.onClose}
-        actionLabel="Submit"
-        title="Inesem, mi hogar"
-    />
+    const onBack = () => {
+        setStep((value) => value - 1);
+    }
+
+    const onNext = () => {
+        setStep((value) => value + 1);
+    }
+
+    const actionLabel = useMemo(() => {
+        if (step === STEPS.PRICE) {
+            return 'Create'
+        }
+
+        return 'Next'
+    }, [step]);
+
+    const secondaryActionLabel = useMemo(() => {
+        if (step === STEPS.CATEGORY) {
+          return undefined
+        }
+    
+        return 'Back'
+      }, [step]);
+
+
+    return (
+        <Modal
+            isOpen={rentModal.isOpen}
+            onClose={rentModal.onClose}
+            onSubmit={rentModal.onClose}
+            actionLabel="Submit"
+            title="Inesem, mi hogar"
+        />
     );
 }
 
