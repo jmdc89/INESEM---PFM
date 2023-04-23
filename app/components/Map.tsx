20adaecfe43924a8420a982097d79f4c5a9399de
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-04-23 15:08:00 +0300

    Update Map.tsx
    url

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -20,6 +20,10 @@ interface MapProps {
     center?: number[]
 }
 
+const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+
 
 const Map: React.FC<MapProps> = ({ center }) => {
     return (
@@ -29,7 +33,13 @@ const Map: React.FC<MapProps> = ({ center }) => {
             scrollWheelZoom={false}
             className="h-[35vh] rounded-lg"
         >
-
+            <TileLayer
+          url={url}
+          attribution={attribution}
+        />
+        {center && (
+          <Marker position={center as L.LatLngExpression} />
+        )}
         </MapContainer>
     )
 }
