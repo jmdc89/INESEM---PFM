be90bfa5b9105050400e1f2f990b0c24905a4ea8
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-04-23 14:23:54 +0300

    Update CountrySelect.tsx
    return

--- app/components/inputs/CountrySelect.tsx
+++ app/components/inputs/CountrySelect.tsx
@@ -22,10 +22,45 @@ const CountrySelect: React.FC<CountrySelectProps> = ({
     onChange
 }) => {
 
+    const { getAll } = useCountries();
 
     return (
-        <div></div>
-    )
-}
-
-export default CountrySelect;
+        <div>
+           <Select 
+            placeholder="Anywhere"
+            isClearable
+            options={getAll()}
+            value={value}
+            onChange={(value) => onChange(value as CountrySelectValue)}
+            formatOptionLabel={(option: any) => (
+                <div className="
+                flex flex-row items-center gap-3">
+                  <div>{option.flag}</div>
+                  <div>
+                    {option.label},
+                    <span className="text-neutral-500 ml-1">
+                      {option.region}
+                    </span>
+                  </div>
+                </div>
+              )}
+              classNames={{
+                control: () => 'p-3 border-2',
+                input: () => 'text-lg',
+                option: () => 'text-lg'
+              }}
+              theme={(theme) => ({
+                ...theme,
+                borderRadius: 6,
+                colors: {
+                  ...theme.colors,
+                  primary: 'black',
+                  primary25: '#ffe4e6'
+                }
+              })}
+            />
+          </div>
+         );
+      }
+       
+      export default CountrySelect;
