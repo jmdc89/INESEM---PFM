20483aa01aa22e4f2d1276f87ed24ba9cd16a3af
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-04-23 14:09:21 +0300

    Update CountrySelect.tsx
    countrySelectValue const

--- app/components/inputs/CountrySelect.tsx
+++ app/components/inputs/CountrySelect.tsx
@@ -1,5 +1,17 @@
 'use client';
 
+import Select from 'react-select'
+
+import useCountries from '@/app/hooks/useCountries';
+
+export type CountrySelectValue = {
+    flag: string;
+    label: string;
+    latlng: number[],
+    region: string;
+    value: string
+  }
+
 const CountrySelect = () => {
     return (
         <div></div>
