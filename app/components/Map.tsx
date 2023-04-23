14070d241c769777e4a907ea9caedca22a17cad6
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-04-23 15:00:15 +0300

    Update Map.tsx
    interface and props

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -8,7 +8,20 @@ import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
-const Map = () => {
+// @ts-ignore
+delete L.Icon.Default.prototype._getIconUrl; 
+L.Icon.Default.mergeOptions({
+    iconUrl: markerIcon.src,
+    iconRetinaUrl: markerIcon2x.src,
+    shadowUrl: markerShadow.src,
+});
+
+interface MapProps {
+    center?: number[]
+  }
+
+
+  const Map: React.FC<MapProps> = ({ center }) => {
     return (
         <div></div>
     )
