fb2a650d6ac722f8928d1c14a538e9f455f120d4
José Manuel Domínguez <68101685+jmdc89@users.noreply.github.com>
2023-06-30 16:13:12 +0200

    Create Counter.tsx
    interface and imports

---
+++ app/components/inputs/Counter.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { useCallback } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+interface CounterProps {
+  title: string;
+  subtitle: string;
+  value: number;
+  onChange: (value: number) => void;
+}
+
+const Counter = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default Counter;
